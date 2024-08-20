cd2a88e974e5a6e9c2ce37d3ccc85bb045862498
Artyom Tyshkevich <frod_1999@mail.ru>
2024-08-20 13:01:18 +0300

    Add project files.

---
+++ Book/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain
+{
+    internal class Author
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = "";
+        public DateOnly Bithday { get; set; }
+        public string Country { get; set; } = "";
+    }
+}
